8fe8c250853173fa63fd6c23971b1d45aca0953f
Arpit Chakladar <54011232+arpitchakladar@users.noreply.github.com>
2025-11-28 02:57:51 +0530

    Fixed nvidia + amd graphics drivers.

--- modules/drivers/nvidia.nix
+++ modules/drivers/nvidia.nix
@@ -6,23 +6,23 @@
 	};
 
 	config = lib.mkIf config.drivers.nvidia.enable {
-		services.xserver.videoDrivers = [ "nvidia" "amdgpu" ];
+		services.xserver.videoDrivers = [ "amdgpu" "nvidia" ];
 
 		hardware.nvidia = {
+			open = true;
 			modesetting.enable = true;
-
+			dynamicBoost.enable = true;
 			powerManagement.enable = true;
 			powerManagement.finegrained = true;
-
 			nvidiaSettings = true;
-
-			open = false;
-
-			package = config.boot.kernelPackages.nvidiaPackages.latest;
+			package = config.boot.kernelPackages.nvidiaPackages.stable;
 
 			prime = {
-				offload.enable = true;
-				intelBusId = "PCI:5:0:0";
+				offload = {
+					enable = true;
+					enableOffloadCmd = true;
+				};
+				amdgpuBusId = "PCI:5:0:0";
 				nvidiaBusId = "PCI:1:0:0";
 			};
 		};
