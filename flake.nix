8f8e3d5b6ce4215cd2a23449c895b208bfe13ef1
Arpit Chakladar <arpitchakladar@proton.me>
2026-03-12 23:07:05 +0530

    Added git commit hook to auto run nix fmt before commit

--- flake.nix
+++ flake.nix
@@ -4,16 +4,27 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     base16.url = "github:SenchoPens/base16.nix";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
   };
 
   outputs =
-    { self, nixpkgs, ... }@inputs:
+    { self, nixpkgs, pre-commit-hooks, ... }@inputs:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in
     {
+      checks.${system}.pre-commit-check = pre-commit-hooks.lib.${system}.run {
+        src = ./.;
+        hooks = {
+          nixfmt.enable = true;
+        };
+      };
       formatter.${system} = pkgs.nixfmt-tree;
+      devShells.${system}.default = pkgs.mkShell {
+        inherit (self.checks.${system}.pre-commit-check) shellHook;
+        buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
+      };
 
       nixosConfigurations = {
         bertor = nixpkgs.lib.nixosSystem {
