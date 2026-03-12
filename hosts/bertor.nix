4d540a209b1779ec540e070ddaa5b43eec406bcd
Arpit Chakladar <arpitchakladar@proton.me>
2026-03-12 22:51:04 +0530

    Performed formatting

--- hosts/bertor.nix
+++ hosts/bertor.nix
@@ -1,38 +1,40 @@
 { ... }:
 
 {
-	nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
-	baseDirectory = "/etc/nixos";
+  baseDirectory = "/etc/nixos";
 
-	audio.enable = true;
+  audio.enable = true;
 
-	networking.enable = true;
-	networking.host = "bertor";
-	networking.bluetooth.enable = true;
-	networking.wifi.enable = true;
+  networking.enable = true;
+  networking.host = "bertor";
+  networking.bluetooth.enable = true;
+  networking.wifi.enable = true;
 
-	time.timeZone = "Asia/Kolkata";
+  time.timeZone = "Asia/Kolkata";
 
-	powerManagement.enable = true;
+  powerManagement.enable = true;
 
-	desktop.enable = true;
+  desktop.enable = true;
 
-	drivers.nvidia.enable = true;
-	drivers.nvidia.amdgpuBusId = "PCI:5:0:0";
-	drivers.nvidia.nvidiaBusId = "PCI:1:0:0";
+  drivers.nvidia.enable = true;
+  drivers.nvidia.amdgpuBusId = "PCI:5:0:0";
+  drivers.nvidia.nvidiaBusId = "PCI:1:0:0";
 
-	user.users = [
-		{
-			username = "arpit";
-			wheel = true;
-		}
-	];
+  user.users = [
+    {
+      username = "arpit";
+      wheel = true;
+    }
+  ];
 
-	virtualization.enable = true;
+  virtualization.enable = true;
 
-	nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfree = true;
 
-	system.stateVersion = "25.05";
+  system.stateVersion = "25.05";
 }
-
--- modules/user/default.nix
+++ modules/user/default.nix
@@ -1,56 +1,66 @@
-{ lib, config, pkgs, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 
 {
   options.user = {
     users = lib.mkOption {
-      type = lib.types.listOf (lib.types.submodule {
-        options = {
-          username = lib.mkOption {
-            type = lib.types.str;
-            description = "Username of the user.";
-          };
+      type = lib.types.listOf (
+        lib.types.submodule {
+          options = {
+            username = lib.mkOption {
+              type = lib.types.str;
+              description = "Username of the user.";
+            };
 
-          wheel = lib.mkEnableOption "Set whether the user has sudo priviledges.";
+            wheel = lib.mkEnableOption "Set whether the user has sudo priviledges.";
 
-          extraGroups = lib.mkOption {
-            type = lib.types.listOf lib.types.str;
-            default = [];
-            description = "Extra groups for the user (in addition to automatic ones).";
+            extraGroups = lib.mkOption {
+              type = lib.types.listOf lib.types.str;
+              default = [ ];
+              description = "Extra groups for the user (in addition to automatic ones).";
+            };
           };
-        };
-      });
+        }
+      );
       description = "List of user accounts to configure.";
     };
   };
 
   config = {
-    users.users = lib.listToAttrs (map (user: {
-      name = user.username;
-      value = {
-        isNormalUser = true;
-        initialPassword = "nixos";
-        shell = pkgs.zsh;
-        extraGroups = lib.mkMerge [
-          (lib.mkIf user.wheel
-            (lib.mkMerge [
-              [ "wheel" "input" ]
-              (if config.audio.enable then [ "audio" ] else [])
-              (if config.hardware.graphics.enable then [ "video" ] else [])
-              (if config.virtualization.enable then [ "libvirtd" ] else [])
-            ])
-          )
-          user.extraGroups
-        ];
-      };
-    }) config.user.users);
+    users.users = lib.listToAttrs (
+      map (user: {
+        name = user.username;
+        value = {
+          isNormalUser = true;
+          initialPassword = "nixos";
+          shell = pkgs.zsh;
+          extraGroups = lib.mkMerge [
+            (lib.mkIf user.wheel (
+              lib.mkMerge [
+                [
+                  "wheel"
+                  "input"
+                ]
+                (if config.audio.enable then [ "audio" ] else [ ])
+                (if config.hardware.graphics.enable then [ "video" ] else [ ])
+                (if config.virtualization.enable then [ "libvirtd" ] else [ ])
+              ]
+            ))
+            user.extraGroups
+          ];
+        };
+      }) config.user.users
+    );
 
-    nix.settings.trusted-users = (lib.mkMerge [
-      ["root"]
-      (map
-        (user: lib.mkIf user.wheel user.username)
-        config.user.users
-      )
-    ]);
+    nix.settings.trusted-users = (
+      lib.mkMerge [
+        [ "root" ]
+        (map (user: lib.mkIf user.wheel user.username) config.user.users)
+      ]
+    );
   };
 }
-
